26b2af132bcd9afa3b4a5648be914d09d9ae7f7e
cjwit <chris.witulski@gmail.com>
2021-07-18 14:05:11 -0400

    remove duplicate error validation message (blank)

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,5 @@
 class Image < ApplicationRecord
     has_one_attached :imageFile, :dependent => :destroy
-    validates :title, presence: true, length: {minimum: 3, maximum: 50 }
+    validates :title, length: { minimum: 3, maximum: 50 }
     validates :imageFile, presence: true
 end
