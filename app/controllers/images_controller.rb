0181e811281fb003cfd2b5d6c875125951bdfccc
cjwit <chris.witulski@gmail.com>
2021-07-17 23:27:59 -0400

    set up basic new image form (text only)

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,4 +1,23 @@
 class ImagesController < ApplicationController
   def index
+    @images = Image.all
   end
+
+  def new
+    @image = Image.new
+  end
+
+  def create
+    @image = Image.new(image_params)
+    if @image.save
+      redirect_to root
+    else
+      render :new
+    end
+  end
+
+  private
+    def image_params
+      params.require(:image).permit(:title, :file)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root "images#index"
-  get "/images", to: "images#index"
+  resources :images
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
