cc354fc601d4463f1218ae86ca41ddfa74b1315e
cjwit <chris.witulski@gmail.com>
2021-07-18 18:13:09 -0400

    render gallery with resized images

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,4 +2,13 @@ class Image < ApplicationRecord
     has_one_attached :imageFile, :dependent => :destroy
     validates :title, length: { minimum: 3, maximum: 50 }
     validates :imageFile, presence: true
+    validate :correct_document_mime_type
+    
+    
+    private
+    def correct_document_mime_type
+        if imageFile.attached? && !imageFile.content_type.in?(%w("image/jpeg"))
+            errors.add(:imageFile, "Only JPEG files are accepted.")
+        end
+    end
 end
