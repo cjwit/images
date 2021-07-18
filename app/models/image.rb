d9226bfd953cc97f1d60d7e63737967c556301c0
cjwit <chris.witulski@gmail.com>
2021-07-18 19:19:00 -0400

    add validation in form and model using mime-type

--- app/models/image.rb
+++ app/models/image.rb
@@ -3,12 +3,12 @@ class Image < ApplicationRecord
     validates :title, length: { minimum: 3, maximum: 50 }
     validates :imageFile, presence: true
     validate :correct_document_mime_type
-    
+    ACCEPTED_FORMATS = ["image/jpg", "image/png", "image/gif"]
     
     private
     def correct_document_mime_type
-        if imageFile.attached? && !imageFile.content_type.in?(%w("image/jpeg"))
-            errors.add(:imageFile, "Only JPEG files are accepted.")
+        if imageFile.attached? && !imageFile.content_type.in?(ACCEPTED_FORMATS)
+            errors.add(:imageFile, "Only JPEG, PNG, and GIF files are accepted.")
         end
     end
 end
