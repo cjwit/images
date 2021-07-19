d1a76808d8416b93b3dfb9166adad0b8c30b16ac
cjwit <chris.witulski@gmail.com>
2021-07-18 20:14:42 -0400

    remove image/jpg from mime types (only need jpeg)

--- app/models/image.rb
+++ app/models/image.rb
@@ -3,7 +3,7 @@ class Image < ApplicationRecord
     validates :title, length: { minimum: 3, maximum: 50 }
     validates :imageFile, presence: true
     validate :correct_document_mime_type
-    ACCEPTED_FORMATS = ["image/jpg", "image/png", "image/gif", "image/jpeg"]
+    ACCEPTED_FORMATS = ["image/png", "image/gif", "image/jpeg"]
     
     private
     def correct_document_mime_type
