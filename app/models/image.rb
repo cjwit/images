d30d4def7e7d4ba097ba80f24254a9135f201795
cjwit <chris.witulski@gmail.com>
2021-07-18 20:12:52 -0400

    fix jpeg mime type bug

--- app/models/image.rb
+++ app/models/image.rb
@@ -3,12 +3,13 @@ class Image < ApplicationRecord
     validates :title, length: { minimum: 3, maximum: 50 }
     validates :imageFile, presence: true
     validate :correct_document_mime_type
-    ACCEPTED_FORMATS = ["image/jpg", "image/png", "image/gif"]
+    ACCEPTED_FORMATS = ["image/jpg", "image/png", "image/gif", "image/jpeg"]
     
     private
     def correct_document_mime_type
         if imageFile.attached? && !imageFile.content_type.in?(ACCEPTED_FORMATS)
-            errors.add(:imageFile, "Only JPEG, PNG, and GIF files are accepted.")
+            puts imageFile.content_type
+            errors.add(:imageFile, "Only JPEG, PNG, GIF, and HIEC/HIEF files are accepted.")
         end
     end
 end
